2618fb031b3fdd088f2905bef7d12d7789ba3eaf
unknown00p <rajpoint8976@gmail.com>
2025-04-29 13:41:40 +0530

    ....

--- backend/src/app.js
+++ backend/src/app.js
@@ -29,7 +29,7 @@ const corsOptions = {
 }
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', process.env.CORS_ORIGIN);
+    res.header('Access-Control-Allow-Origin', 'youtube-app-ecru.vercel.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
